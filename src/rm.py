455e90d98f9a0a4b4b7f188a14219d320c19894b
Siew Kam Onn <kosiew@gmail.com>
2024-06-24 14:11:37 +0800

    Add OpenAIBrowserSearch for retrieval

--- src/rm.py
+++ src/rm.py
@@ -1,9 +1,10 @@
 import logging
 import os
-from typing import Callable, Union, List
+from typing import Callable, List, Union
 
 import dspy
 import requests
+from openai import OpenAI
 
 from utils import WebPageHelper
 
@@ -12,7 +13,9 @@ class YouRM(dspy.Retrieve):
     def __init__(self, ydc_api_key=None, k=3, is_valid_source: Callable = None):
         super().__init__(k=k)
         if not ydc_api_key and not os.environ.get("YDC_API_KEY"):
-            raise RuntimeError("You must supply ydc_api_key or set environment variable YDC_API_KEY")
+            raise RuntimeError(
+                "You must supply ydc_api_key or set environment variable YDC_API_KEY"
+            )
         elif ydc_api_key:
             self.ydc_api_key = ydc_api_key
         else:
@@ -29,9 +32,11 @@ class YouRM(dspy.Retrieve):
         usage = self.usage
         self.usage = 0
 
-        return {'YouRM': usage}
+        return {"YouRM": usage}
 
-    def forward(self, query_or_queries: Union[str, List[str]], exclude_urls: List[str] = []):
+    def forward(
+        self, query_or_queries: Union[str, List[str]], exclude_urls: List[str] = []
+    ):
         """Search with You.com for self.k top passages for query or queries
 
         Args:
@@ -57,21 +62,30 @@ class YouRM(dspy.Retrieve):
                 ).json()
 
                 authoritative_results = []
-                for r in results['hits']:
-                    if self.is_valid_source(r['url']) and r['url'] not in exclude_urls:
+                for r in results["hits"]:
+                    if self.is_valid_source(r["url"]) and r["url"] not in exclude_urls:
                         authoritative_results.append(r)
-                if 'hits' in results:
-                    collected_results.extend(authoritative_results[:self.k])
+                if "hits" in results:
+                    collected_results.extend(authoritative_results[: self.k])
             except Exception as e:
-                logging.error(f'Error occurs when searching query {query}: {e}')
+                logging.error(f"Error occurs when searching query {query}: {e}")
 
         return collected_results
 
 
 class BingSearch(dspy.Retrieve):
-    def __init__(self, bing_search_api_key=None, k=3, is_valid_source: Callable = None,
-                 min_char_count: int = 150, snippet_chunk_size: int = 1000, webpage_helper_max_threads=10,
-                 mkt='en-US', language='en', **kwargs):
+    def __init__(
+        self,
+        bing_search_api_key=None,
+        k=3,
+        is_valid_source: Callable = None,
+        min_char_count: int = 150,
+        snippet_chunk_size: int = 1000,
+        webpage_helper_max_threads=10,
+        mkt="en-US",
+        language="en",
+        **kwargs,
+    ):
         """
         Params:
             min_char_count: Minimum character count for the article to be considered valid.
@@ -83,22 +97,18 @@ class BingSearch(dspy.Retrieve):
         super().__init__(k=k)
         if not bing_search_api_key and not os.environ.get("BING_SEARCH_API_KEY"):
             raise RuntimeError(
-                "You must supply bing_search_subscription_key or set environment variable BING_SEARCH_API_KEY")
+                "You must supply bing_search_subscription_key or set environment variable BING_SEARCH_API_KEY"
+            )
         elif bing_search_api_key:
             self.bing_api_key = bing_search_api_key
         else:
             self.bing_api_key = os.environ["BING_SEARCH_API_KEY"]
         self.endpoint = "https://api.bing.microsoft.com/v7.0/search"
-        self.params = {
-            'mkt': mkt,
-            "setLang": language,
-            "count": k,
-            **kwargs
-        }
+        self.params = {"mkt": mkt, "setLang": language, "count": k, **kwargs}
         self.webpage_helper = WebPageHelper(
             min_char_count=min_char_count,
             snippet_chunk_size=snippet_chunk_size,
-            max_thread_num=webpage_helper_max_threads
+            max_thread_num=webpage_helper_max_threads,
         )
         self.usage = 0
 
@@ -112,9 +122,11 @@ class BingSearch(dspy.Retrieve):
         usage = self.usage
         self.usage = 0
 
-        return {'BingSearch': usage}
+        return {"BingSearch": usage}
 
-    def forward(self, query_or_queries: Union[str, List[str]], exclude_urls: List[str] = []):
+    def forward(
+        self, query_or_queries: Union[str, List[str]], exclude_urls: List[str] = []
+    ):
         """Search with Bing for self.k top passages for query or queries
 
         Args:
@@ -138,22 +150,176 @@ class BingSearch(dspy.Retrieve):
         for query in queries:
             try:
                 results = requests.get(
-                    self.endpoint,
-                    headers=headers,
-                    params={**self.params, 'q': query}
+                    self.endpoint, headers=headers, params={**self.params, "q": query}
                 ).json()
 
-                for d in results['webPages']['value']:
-                    if self.is_valid_source(d['url']) and d['url'] not in exclude_urls:
-                        url_to_results[d['url']] = {'url': d['url'], 'title': d['name'], 'description': d['snippet']}
+                for d in results["webPages"]["value"]:
+                    if self.is_valid_source(d["url"]) and d["url"] not in exclude_urls:
+                        url_to_results[d["url"]] = {
+                            "url": d["url"],
+                            "title": d["name"],
+                            "description": d["snippet"],
+                        }
             except Exception as e:
-                logging.error(f'Error occurs when searching query {query}: {e}')
+                logging.error(f"Error occurs when searching query {query}: {e}")
 
-        valid_url_to_snippets = self.webpage_helper.urls_to_snippets(list(url_to_results.keys()))
+        valid_url_to_snippets = self.webpage_helper.urls_to_snippets(
+            list(url_to_results.keys())
+        )
         collected_results = []
         for url in valid_url_to_snippets:
             r = url_to_results[url]
-            r['snippets'] = valid_url_to_snippets[url]['snippets']
+            r["snippets"] = valid_url_to_snippets[url]["snippets"]
+            collected_results.append(r)
+
+        return collected_results
+
+
+class Chat:
+    def __init__(self, openai_api_key=None, model="gpt-4o"):
+        """
+        Initializes the Chat class with API key and model.
+        """
+        api_key = None
+        if not openai_api_key and not os.environ.get("OPENAI_API_KEY"):
+            raise RuntimeError(
+                "You must supply openai_api_key or set environment variable OPENAI_API_KEY"
+            )
+        elif openai_api_key:
+            api_key = openai_api_key
+        else:
+            api_key = os.environ["OPENAI_API_KEY"]
+
+        self.client = OpenAI(api_key=api_key)
+        self.model = model
+        self.chat_history = [
+            {
+                "role": "system",
+                "content": "You are a helpful assistant designed to output JSON.",
+            }
+        ]
+
+    def append_response_to_history(self, response):
+        message = response.choices[0].message
+
+        self.chat_history.append({"role": message.role, "content": message.content})
+
+    def print_reply(self, response):
+        print(response.choices[0].message.content)
+
+    def ask(self, query, model=model_gpt4o):
+        self.chat_history.append({"role": "user", "content": query})
+
+        response = self.client.chat.completions.create(
+            model=model,
+            response_format={"type": "json_object"},
+            messages=self.chat_history,
+        )
+
+        self.append_response_to_history(response)
+
+        self.print_reply(response)
+        print(response.usage.total_tokens)
+
+    def browser_results(self, query, k=3):
+        _query = f"Use Browser to retrieve {k} results related to {query}. The output should be a results array of dict of (title, url, snippet)"
+        return self.ask(_query)
+
+
+class OpenAIBrowserSearch(dspy.Retrieve):
+    model = "gpt-4o"
+
+    def __init__(
+        self,
+        openai_api_key=None,
+        k=3,
+        is_valid_source: Callable = None,
+        min_char_count: int = 150,
+        snippet_chunk_size: int = 1000,
+        webpage_helper_max_threads=10,
+        **kwargs,
+    ):
+        """
+        Params:
+            min_char_count: Minimum character count for the article to be considered valid.
+            snippet_chunk_size: Maximum character count for each snippet.
+            webpage_helper_max_threads: Maximum number of threads to use for webpage helper.
+            **kwargs: Additional parameters for the OpenAI API.
+        """
+        super().__init__(k=k)
+        if not openai_api_key and not os.environ.get("OPENAI_API_KEY"):
+            raise RuntimeError(
+                "You must supply openai_api_key or set environment variable OPENAI_API_KEY"
+            )
+        elif openai_api_key:
+            self.openai_api_key = openai_api_key
+        else:
+            self.openai_api_key = os.environ["OPENAI_API_KEY"]
+
+        self.k = k
+        self.webpage_helper = WebPageHelper(
+            min_char_count=min_char_count,
+            snippet_chunk_size=snippet_chunk_size,
+            max_thread_num=webpage_helper_max_threads,
+        )
+        self.usage = 0
+
+        # If not None, is_valid_source shall be a function that takes a URL and returns a boolean.
+        if is_valid_source:
+            self.is_valid_source = is_valid_source
+        else:
+            self.is_valid_source = lambda x: True
+        self.chat = Chat(openai_api_key=self.openai_api_key, model=self.model)
+
+    # TODO: this is used in BingSearch and YouRM, consider moving it to a common place.
+    def get_usage_and_reset(self):
+        usage = self.usage
+        self.usage = 0
+        return {"OpenAIBrowserSearch": usage}
+
+    # this is the important function that is used by Retriever in the pipeline
+    def forward(
+        self, query_or_queries: Union[str, List[str]], exclude_urls: List[str] = []
+    ):
+        """Search with OpenAI's Browser Tool for self.k top passages for query or queries
+
+        Args:
+            query_or_queries (Union[str, List[str]]): The query or queries to search for.
+            exclude_urls (List[str]): A list of urls to exclude from the search results.
+
+        Returns:
+            a list of Dicts, each dict has keys of 'description', 'snippets' (list of strings), 'title', 'url'
+        """
+        queries = (
+            [query_or_queries]
+            if isinstance(query_or_queries, str)
+            else query_or_queries
+        )
+        self.usage += len(queries)
+
+        url_to_results = {}
+
+        for query in queries:
+            try:
+                results = self.chat.browser_results(query, self.k)
+
+                for d in results["results"]:
+                    if self.is_valid_source(d["url"]) and d["url"] not in exclude_urls:
+                        url_to_results[d["url"]] = {
+                            "url": d["url"],
+                            "title": d["title"],
+                            "description": d["snippet"],
+                        }
+            except Exception as e:
+                logging.error(f"Error occurs when searching query {query}: {e}")
+
+        valid_url_to_snippets = self.webpage_helper.urls_to_snippets(
+            list(url_to_results.keys())
+        )
+        collected_results = []
+        for url in valid_url_to_snippets:
+            r = url_to_results[url]
+            r["snippets"] = valid_url_to_snippets[url]["snippets"]
             collected_results.append(r)
 
         return collected_results
